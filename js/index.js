9ac1c20e990a0b4ec7239ef163687bc80dcd0d0c
SolchuPera <solchuper77@gmail.com>
2024-08-23 10:30:11 -0300

    Update index.js
    Agregué código (Event listener) en las líneas 17 a 23 que sirve para verificar si el usuario está autenticado, lo necesito para otros archivos de código

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+//manejar los clicks en las categorías
 document.addEventListener("DOMContentLoaded", function(){
     document.getElementById("autos").addEventListener("click", function() {
         localStorage.setItem("catID", 101);
@@ -12,3 +13,11 @@ document.addEventListener("DOMContentLoaded", function(){
         window.location = "products.html"
     });
 });
+
+document.addEventListener("DOMContentLoaded", function(){ //Verificamos si el usuario está autenticado
+    let usuario = localStorage.getItem("user");
+    if (usuario === null){ //Si no hay un usuario en el localStoragen nos manda a login.html
+        window.location.href = "login.html";
+        return; //sale de la función para que no se ejecute el resto del código
+    }
+})

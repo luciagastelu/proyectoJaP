c3e1ba7a0a50c2b865e7f28dec39bf34e5d08d13
SolchuPera <solchuper77@gmail.com>
2024-08-27 17:11:24 -0300

    Update login.js para mostrar mail al iniciar sesión
    Cambié el localStorage de modo que se guarde el usuario (el mail) para poder mostrarse al momento de iniciar la sesión en la barra superior.

--- js/login.js
+++ js/login.js
@@ -1,4 +1,3 @@
-
 function showAlertError(){
     alert("Por favor, completa todos los campos antes de enviar el formulario.");
 }
@@ -21,35 +20,15 @@ function validarForm(event){
 
     // Revisa que los campos no estan vacios
     if (username === "" || password === "") {
-
-      function validarForm() {
-    const username = document.getElementById("usuario").value;
-    const password = document.getElementById("contraseña").value;
-    
-    // Revisa que los campos no están vacíos
-    if (username === "" || password === "") {
         // Mostrar el error
         showAlertError();
     } else { 
-        // Guardar la sesión
-        localStorage.setItem('user', JSON.stringify({username: username }));
         // Mostrar alerta de éxito
         showAlertSuccess();
-        
-        // Redirigir después de 2 segundos
-        setTimeout(() => {
-            window.location.href = "index.html";
-        }, 2000); 
-    }
+         // Guardar la sesión        
+         localStorage.setItem('userEmail', username);
+        // Redirige al inicio
+        window.location.href = "index.html";
+    
 }
-
-function showAlertError() {
-    alert("Por favor, completa todos los campos antes de enviar el formulario.");
 }
-
-function showAlertSuccess() {
-    alert("¡Inicio de sesión exitoso!");
-}
-
-// Event listener al botón de ingresar
-document.getElementById('regBtn').addEventListener('click', validarForm);
